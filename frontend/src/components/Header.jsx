c5e12f81921d4b002e4a4e7cc5593a980a4de783
shanmukh-sai2003 <koradashanmukhasai10@gmail.com>
2024-05-01 10:35:07 +0530

    Update header to show logout and admin only after login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import useAuth from "../utils/useAuth";
 
 function Header() {
+    const { user } = useAuth();
+
     return (
         <header className="py-6 px-[10vw] flex justify-between mt-2 border-b-2">
             <h1 className="text-6xl font-bold"><Link to={`/`}>Blogsly</Link></h1>
             <ul className="flex gap-4 text-xl items-center">
-                <li><Link to={`/login`}>login</Link></li>
-                <li><Link to={`/logout`}>logout</Link></li>
-                <li><Link to={`/admin`}>admin</Link></li>
+                { !user?.accessToken && <li><Link to={`/login`}>login</Link></li> }
+                { user?.accessToken && <li><Link to={`/logout`}>logout</Link></li> }
+                { user?.accessToken && <li><Link to={`/admin`}>admin</Link></li> }
             </ul>
         </header>
     );
